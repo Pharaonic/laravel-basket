6eb86ea41f6ac908186240391f34d74106fdc0be
Moamen Eltouny <raggigroup@gmail.com>
2022-09-29 21:47:45 +0200

    Add method of addition in BasketManager

--- src/BasketManager.php
+++ src/BasketManager.php
@@ -3,10 +3,9 @@
 namespace Pharaonic\Laravel\Basket;
 
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Str;
 use Pharaonic\Laravel\Basket\Exceptions\BasketNotFoundException;
 use Pharaonic\Laravel\Basket\Exceptions\BasketUnauthorizedException;
 use Pharaonic\Laravel\Basket\Models\Basket;
@@ -15,215 +14,274 @@ use Pharaonic\Laravel\Basket\Traits\isCustomer;
 
 class BasketManager
 {
-    /**
-     * Basket Model
-     *
-     * @var Basket|null
-     */
-    private $basket;
+	/**
+	 * Basket Model
+	 *
+	 * @var Basket|null
+	 */
+	private $basket;
 
-    /**
-     * Basket Configurations
-     *
-     * @var array
-     */
-    private $config;
+	/**
+	 * Basket Configurations
+	 *
+	 * @var array
+	 */
+	private $config;
 
-    public function __construct()
-    {
-        $this->config = config('Pharaonic.basket');
+	public function __construct()
+	{
+		$this->config = config('Pharaonic.basket');
 
-        // Use the current basket automatically (Web Only)
-        if ($this->config['auto_detect'] && !request()->wantsJson()) {
-            if ($id = Session::isStarted() ? Session::get('basket_id') : Cookie::get('basket_id')) {
-                $this->use($id);
-            }
-        }
-    }
+		// Use the current basket automatically (Web Only)
+		if ($this->config['auto_detect'] && !request()->wantsJson()) {
+			if ($id = Session::isStarted() ? Session::get('basket_id') : Cookie::get('basket_id')) {
+				$this->use($id);
+			}
+		}
+	}
 
-    public function __get($name)
-    {
-        $name = 'get' . Str::studly($name) . 'Attribute';
+	/**
+	 * Use a specific basket through id.
+	 *
+	 * @param string $id
+	 * @return $this
+	 */
+	public function use(string $id)
+	{
+		if (!($basket = Basket::with(['items.modelable'])->find($id))) {
+			throw new BasketNotFoundException('Basket has not found');
+		}
 
-        if (method_exists($this, $name)) return $this->{$name};
+		// Check Authorization
+		if (
+			$basket->user_agent && $basket->user_agent != request()->server('HTTP_USER_AGENT') ||
+			(($user = auth()->user()) && !$basket->user_agent && ($user::class != $basket->user_type || $user->id != $basket->user_id))
+		) {
+			throw new BasketUnauthorizedException('You are not authorized to use this basket.');
+		}
 
-        return null;
-    }
+		$this->basket = $basket;
 
-    /**
-     * Get items count
-     *
-     * @return int
-     */
-    public function getCountAttribute()
-    {
-        if (!$this->basket) return 0;
+		// Assign the user to the basket automatically.
+		if ($user && in_array(isCustomer::class, class_uses($user)) && $basket->user_agent) {
+			$this->assignUser($user);
+		}
 
-        return $this->basket->items->count();
-    }
+		return $this;
+	}
 
-    /**
-     * Use a specific basket through id.
-     *
-     * @param string $id
-     * @return $this
-     */
-    public function use(string $id)
-    {
-        if (!($basket = Basket::with(['items.modelable'])->find($id))) {
-            throw new BasketNotFoundException('Basket has not found');
-        }
+	/**
+	 * Assign a user to the basket.
+	 *
+	 * @param isCustomer $user
+	 * @return $this
+	 */
+	public function assignUser(isCustomer $user)
+	{
+		$basket->user_agent = null;
+		$basket->user()->associate($user);
+		$basket->save();
 
-        // Check Authorization
-        if (
-            $basket->user_agent && $basket->user_agent != request()->server('HTTP_USER_AGENT') ||
-            (($user = auth()->user()) && !$basket->user_agent && ($user::class != $basket->user_type || $user->id != $basket->user_id))
-        ) {
-            throw new BasketUnauthorizedException('You are not authorized to use this basket.');
-        }
+		return $this;
+	}
 
-        $this->basket = $basket;
+	/**
+	 * Create a new basket.
+	 *
+	 * @param string|null $currency
+	 * @param string|null $user_agent
+	 * @return $this
+	 */
+	public function create(string $currency = null, string $user_agent = null)
+	{
+		if ($user = auth()->user()) {
+			$this->basket = Basket::create(['currency' => $currency ?? $this->config['currency']]);
+			$this->basket->user()->associate($user)->save();
+		} else {
+			$this->basket = Basket::create([
+				'currency'      => $currency ?? $this->config['currency'],
+				'user_agent'    => $user_agent ?? request()->server('HTTP_USER_AGENT')
+			]);
+		}
 
-        // Assign the user to the basket automatically.
-        if ($user && in_array(isCustomer::class, class_uses($user)) && $basket->user_agent) {
-            $this->assignUser($user);
-        }
+		// Store For Web ONLY
+		if ($this->config['auto_detect'] && !request()->wantsJson()) {
+			if (Session::isStarted()) {
+				Session::put('basket_id', $this->basket->id);
+			} else {
+				Cookie::queue(cookie()->forever('basket_id', $this->basket->id));
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Create a new basket.
-     *
-     * @param string|null $currency
-     * @param string|null $user_agent
-     * @return $this
-     */
-    public function create(string $currency = null, string $user_agent = null)
-    {
-        if ($user = auth()->user()) {
-            $this->basket = Basket::create(['currency' => $currency ?? $this->config['currency']]);
-            $this->basket->user()->associate($user)->save();
-        } else {
-            $this->basket = Basket::create([
-                'currency'      => $currency ?? $this->config['currency'],
-                'user_agent'    => $user_agent ?? request()->server('HTTP_USER_AGENT')
-            ]);
-        }
+	/**
+	 * Destroy the current basket.
+	 *
+	 * @return void
+	 */
+	public function destroy()
+	{
+		if (!$this->basket) return;
 
-        // Store For Web ONLY
-        if ($this->config['auto_detect'] && !request()->wantsJson()) {
-            if (Session::isStarted()) {
-                Session::put('basket_id', $this->basket->id);
-            } else {
-                Cookie::queue(cookie()->forever('basket_id', $this->basket->id));
-            }
-        }
+		// TODO : Depends on it's status
+		$this->basket->delete();
+		$this->basket = null;
+	}
 
-        return $this;
-    }
+	/**
+	 * Add a new basket item.
+	 *
+	 * @param mixed ...$items
+	 * @return BasketItem|boolean|null
+	 */
+	public function add(...$items)
+	{
+		if (!$this->basket) return;
 
-    /**
-     * Destroy the current basket.
-     *
-     * @return void
-     */
-    public function destroy()
-    {
-        if (!$this->basket) return;
+		if (is_array($items[0])) {
+			$items = $items[0];
+			if (isset($item[0]) && is_array($item[0])) {
+				// Multiple Items
+				foreach ($items as $item) {
+					return $this->insert($item['name'], $item['price'], $item['quantity'] ?? 1, $item['attributes'] ?? null, $item['model'] ?? null);
+				}
+			} else {
+				// Single Item
+				return $this->insert($items['name'], $items['price'], $items['quantity'] ?? 1, $items['attributes'] ?? null, $items['model'] ?? null);
+			}
+		} else {
+			return $this->insert($items[0], $items[1], $items[2] ?? 1, $items[3] ?? null, $items[4] ?? null);
+		}
+	}
 
-        // TODO : Depends on it's status
-        $this->basket->delete();
-        $this->basket = null;
-    }
+	/**
+	 * Insert a new basket item.
+	 *
+	 * @param string $name
+	 * @param float $price
+	 * @param int $quantity
+	 * @param array|null $attributes
+	 * @param Model|null $model
+	 * @return BasketItem
+	 */
+	private function insert(string $name, float $price, int $quantity = 1, array $attributes = null, Model $model = null)
+	{
+		$item = $this->basket->items()->create([
+			'name' => $name,
+			'price' => $price,
+			'quantity' => $quantity,
+			'attributes' => $attributes,
+		]);
 
-    /**
-     * Assign a user to the basket.
-     *
-     * @param isCustomer $user
-     * @return $this
-     */
-    public function assignUser(isCustomer $user)
-    {
-        $basket->user_agent = null;
-        $basket->user()->associate($user);
-        $basket->save();
+		if ($model) {
+			$item->modelable()->associate($model)->save();
+		}
 
-        return $this;
-    }
+		$this->basket->items->append($item);
 
-    /**
-     * Add a new basket item.
-     *
-     * @return BasketItem
-     */
-    public function add()
-    {
-        // 
-    }
+		return $item;
+	}
 
-    /**
-     * Remove a basket item.
-     *
-     * @param  int $identifier
-     * @return bool
-     */
-    public function remove(int $identifier)
-    {
-        // Remove by basket-item index
-        if ($item = $this->find($identifier)) {
-            // TODO : Depends on status
-            $item->delete();
-            $this->basket->items->forget($identifier);
+	/**
+	 * Remove a basket item.
+	 *
+	 * @param  int $identifier
+	 * @return bool
+	 */
+	public function remove(int $identifier)
+	{
+		// Remove by basket-item index
+		if ($item = $this->find($identifier)) {
+			// TODO : Depends on status
+			$item->delete();
+			$this->basket->items->forget($identifier);
 
-            return true;
-        }
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    /**
-     * Clear all basket items.
-     *
-     * @return bool
-     */
-    public function clear()
-    {
-        if (!$this->basket) return false;
+	/**
+	 * Clear all basket items.
+	 *
+	 * @return bool
+	 */
+	public function clear()
+	{
+		if (!$this->basket) return false;
 
-        $this->basket->items()->delete();
-        $this->basket->setRelation('items', new EloquentCollection([]));
+		$this->basket->items()->delete();
+		$this->basket->setRelation('items', new EloquentCollection([]));
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Get a basket item/Items.
-     *
-     * @param int $identifier
-     * @return BasketItem|null
-     */
-    public function find(int $identifier)
-    {
-        if (!$this->basket)
-            throw new BasketNotFoundException('Basket has not found');
+	/**
+	 * Get a basket item/Items.
+	 *
+	 * @param int $identifier
+	 * @return BasketItem|null
+	 */
+	public function find(int $identifier)
+	{
+		if (!$this->basket)
+			throw new BasketNotFoundException('Basket has not found');
 
-        if (!isset($this->basket->items[$identifier]))
-            return;
+		if (!isset($this->basket->items[$identifier]))
+			return;
 
-        return $this->basket->items[$identifier];
-    }
+		return $this->basket->items[$identifier];
+	}
 
-    /**
-     * Get all basket items.
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    public function all()
-    {
-        if (!$this->basket)
-            throw new BasketNotFoundException('Basket has not found');
+	/**
+	 * Get all basket items.
+	 *
+	 * @return \Illuminate\Support\Collection
+	 */
+	public function all()
+	{
+		if (!$this->basket)
+			throw new BasketNotFoundException('Basket has not found');
 
-        return $this->basket->items;
-    }
+		return $this->basket->items;
+	}
+
+	/**
+	 * Get items count
+	 *
+	 * @return int
+	 */
+	public function count()
+	{
+		if (!$this->basket) return 0;
+
+		return $this->basket->items->count();
+	}
+
+	/**
+	 * Check if items list is empty.
+	 *
+	 * @return boolean
+	 */
+	public function isEmpty()
+	{
+		if (!$this->basket) return;
+
+		return $this->basket->items->isEmpty();
+	}
+
+	/**
+	 * Check if items list is not empty.
+	 *
+	 * @return boolean
+	 */
+	public function isNotEmpty()
+	{
+		if (!$this->basket) return;
+
+		return $this->basket->items->isNotEmpty();
+	}
 }
