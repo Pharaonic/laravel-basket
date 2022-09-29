f82203c0a7b391803a52c9abab87c0e58593b6e4
Moamen Eltouny <raggigroup@gmail.com>
2022-09-29 23:24:04 +0200

    Optimze uses

--- src/BasketManager.php
+++ src/BasketManager.php
@@ -2,7 +2,6 @@
 
 namespace Pharaonic\Laravel\Basket;
 
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cookie;
