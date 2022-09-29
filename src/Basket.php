dcef6560dbb35f33add96078427284f8a9fa6b58
Moamen Eltouny <raggigroup@gmail.com>
2022-09-29 12:38:33 +0200

    Start Basket core implementation

--- src/Basket.php
+++ src/Basket.php
@@ -2,11 +2,66 @@
 
 namespace Pharaonic\Laravel\Basket;
 
+use Pharaonic\Laravel\Basket\Models\BasketItem;
+
 class Basket
 {
-    // add
-    // remove
-    // clear
-    // get
-    // find
+    protected $model;
+
+    public function __construct()
+    {
+        // 
+    }
+
+    /**
+     * Add a new basket item.
+     *
+     * @return BasketItem
+     */
+    public function add()
+    {
+        // 
+    }
+
+    /**
+     * Remove a basket item.
+     *
+     * @param \Illuminate\Database\Eloquent\Model|int $identifier
+     * @return bool
+     */
+    public function remove($identifier)
+    {
+        // 
+    }
+
+    /**
+     * Clear all basket items.
+     *
+     * @return bool
+     */
+    public function clear()
+    {
+        // 
+    }
+
+    /**
+     * Get a basket item/Items.
+     *
+     * @param \Illuminate\Database\Eloquent\Model|int $identifier
+     * @return BasketItem|null
+     */
+    public function find($identifier)
+    {
+        // 
+    }
+
+    /**
+     * Get all basket items.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function all()
+    {
+        // 
+    }
 }
